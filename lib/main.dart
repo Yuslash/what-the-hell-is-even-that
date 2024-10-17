9e5b7bf2f9837a971699301a04cea86858296671
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-17 21:03:41 +0530

    Getting to Understand an bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:learning_1/Pages/home.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            // Use a Column to hold both Row and Column
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                // First Row widget
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(10.0),
-                    color: Colors.red[600],
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                  const Text("hello world"),
-                ],
-              ),
-              Column(
-                // Second Column widget
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(10.0),
-                    color: Colors.amber[800],
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
