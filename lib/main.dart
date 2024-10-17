af6d67c8f6e510aac091085c3811804fab75e81f
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-17 17:29:50 +0530

    I dont understand the Damn thing But its wokring first day of conquering mobile app programming

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column( // Use a Column to hold both Row and Column
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row( // First Row widget
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.red[600],
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  const Text("hello world"),
+                ],
+              ),
+              Column( // Second Column widget
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.amber[800],
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
