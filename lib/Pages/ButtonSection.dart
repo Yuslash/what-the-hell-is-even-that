6bf77047d2ba25ba502d3a9c44bb881a7dfdacc0
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-20 23:03:24 +0530

    Getting little Hard

---
+++ lib/Pages/ButtonSection.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    final Color color = Theme.of(context).primaryColor;
+
+  }
+}
