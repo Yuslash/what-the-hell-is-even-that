0b0a398cc5ee39d2c36daae3f55dc9d4316cb555
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-20 23:43:41 +0530

    AHHHHAAHAHHAHAHHAA this is frustrating i can't complete simple thing on my own come i wont giveeeeeeeeeeeeeeee u>>> any lets complete

---
+++ lib/Pages/TextSection.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(32),
+      
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning_1/Pages/ButtonSection.dart';
+import 'package:learning_1/Pages/TextSection.dart';
 import 'package:learning_1/Pages/TitleSection.dart';
 
 void main() {
@@ -29,7 +30,15 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               TitleSection(name: "Sibikrishna Logarajan", location: "Thiruvarur ThiruthuraiPoondi"),
-              ButtonSection()
+              ButtonSection(),
+              TextSection(description: 'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
             ],
           ),
         ),
