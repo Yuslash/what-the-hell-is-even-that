abedbe304fb1841e1adbdc5c84a9d95d7e93b1bd
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-20 23:16:05 +0530

    Completed ButtonWithText

--- lib/Pages/ButtonSection.dart
+++ lib/Pages/ButtonSection.dart
@@ -3,10 +3,40 @@ import 'package:flutter/material.dart';
 class ButtonSection extends StatelessWidget {
   const ButtonSection({super.key});
 
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required color,
+    required icon,
+    required label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+
   @override
   Widget build(BuildContext context) {
- 
-    final Color color = Theme.of(context).primaryColor;
-
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
   }
+
 }
